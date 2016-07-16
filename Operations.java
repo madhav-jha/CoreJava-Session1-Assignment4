73bfcc68c505dea203f5d1941d7d2180f138fb01
madhav-jha <madhavjha2008@gmail.com>
2016-07-17 00:05:09 +0530

    Add files via upload

---
+++ Operations.java
@@ -0,0 +1,17 @@
+package Session1.Assignment4;
+
+public class Operations {
+
+	public static void main(String[] args) {
+		int x=5,y=10,result;
+		result=x + y * 2;
+		System.out.println("result ="+result);
+		result=x - y + 2;
+		System.out.println("result ="+result);
+		result=(x+y) * 2;
+		System.out.println("result ="+result);
+		result=y%x;
+		System.out.println("result ="+result);
+	}
+
+}
